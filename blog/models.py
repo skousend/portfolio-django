0fef594da14a4ebac156070e4e1deef44a1abcf6
Derek Skousen <skousend@gmail.com>
2018-07-21 15:11:21 -0500

    finished portfolio v1

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        wordlist = self.body.split()
+        return ' '.join(wordlist[:15]) + '...'
+        
+#    def pubdatepretty(self):
+#           # see strftime.org for reference
+#        return self.pub_date.strftime('%b %e %Y')
+#   
